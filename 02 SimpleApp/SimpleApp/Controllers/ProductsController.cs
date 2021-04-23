c7959e8fc9b6257d6537717e5017d2e3437ca561
Den251 <denis231251@gmail.com>
2021-04-23 13:46:02 +0300

    Commit

---
+++ 02 SimpleApp/SimpleApp/Controllers/ProductsController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SimpleApp.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SimpleApp.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private ProductReader reader;
+
+        // ВНИМАНИЕ.
+        // Каждый запрос обрабатывает новый экземпляр контроллера.
+        // Конструктор будет вызываться перед вызовом метода List и метода Details
+        // После обработки запроса, экземпляр контроллера будет удален из памяти
+        public ProductsController()
+        {
+            reader = new ProductReader();
+        }
+
+        public IActionResult List(string category)
+        {
+            List<Product> products = reader.ReadFromFile();
+            if (category != null)
+            {
+                List<Product> returnedProducts = new List<Product>();
+
+                foreach (var product in products)
+                {
+                    if (product.Category == category)
+                        returnedProducts.Add(product);
+                }
+
+                return View(returnedProducts);
+
+            }
+            else
+            {
+                return View(products);
+            }
+
+
+        }
+
+        // Products/Details/1
+        public IActionResult Details(int category)
+        {
+            List<Product> products = reader.ReadFromFile();
+            Product product = products.FirstOrDefault(x => x.Id == category);
+
+            if (product != null)
+            {
+                // Возврат представления с именем Details и передача представлению экземпляра product
+                // В представление доступ к экземпляру можно получить через свойство представления Model
+                return View(product);
+            }
+            else
+            {
+                // Возврат ошибки 404
+                return NotFound();
+            }
+        }
+        
+        
+    }
+}
